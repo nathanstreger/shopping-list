583d162e0ade7035209b9927fa46f154e7ea727f
nathanstreger <nathanstreger@gmail.com>
2019-10-20 09:46:48 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var deleteButtons = document.getElementsByClassName("delete");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	//creates new list item delete button
+	var btn = document.createElement("button");
+	btn.innerHTML = "delete";
+	btn.addEventListener("click", deleteItem);
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(btn);
+
+	ul.appendChild(li);
+	input.value = "";
+}
+
+//handles the input by the user.
+function addListAfterClick() {
+	if (inputLength() > 0){
+	createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+	createListElement();
+	}
+}
+
+function addClassName(element, name) {
+	element.classList.add(name);
+}
+
+//click on a list item and it strikethoughs the text
+function getEventTarget(e) {
+	e = e || window.event; //compatible with different browser engines.
+	return e.target || e.srcElement;
+}
+
+function toggleDone(event) {
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+//deletes an item after clicking the delete button next to it.
+function deleteItem(event) {
+	event.target.removeEventListener("click", deleteItem);
+	event.target.parentNode.remove();
+}
+
+//adds event listeners to all the buttons and clickable items.
+for (var i = 0; i < ul.children.length; i++) {
+	deleteButtons[i].addEventListener("click", deleteItem);
+}
+
+ul.addEventListener("click", toggleDone);
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
